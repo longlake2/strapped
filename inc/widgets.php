3562262483931a9d4febde8815eb9067d86b677d
Mike Hart <Mike Hart>
2016-08-22 15:05:45 -0400

    add sidebar panels

--- inc/widgets.php
+++ inc/widgets.php
@@ -1,3 +1,4 @@
+
 <?php
 /**
  * Register widget area.
@@ -5,14 +6,14 @@
  * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
  */
 function strapped_widgets_init() {
-	register_sidebar( array(
-		'name'          => esc_html__( 'Sidebar', 'strapped' ),
-		'id'            => 'sidebar-1',
-		'description'   => esc_html__( 'Add widgets here.', 'strapped' ),
-		'before_widget' => '<section id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</section>',
-		'before_title'  => '<h2 class="widget-title">',
-		'after_title'   => '</h2>',
-	) );
+  register_sidebar( array(
+    'name'          => esc_html__( 'Sidebar', 'strapped' ),
+    'id'            => 'sidebar-1',
+    'description'   => esc_html__( 'Add widgets here.', 'strapped' ),
+    'before_widget' => '<section id="%1$s" class="panel panel-default widget %2$s">',
+    'after_widget'  => '</section>',
+    'before_title'  => '<div class="panel-heading"><h3 class="panel-title widget-title">',
+    'after_title'   => '</h3></div>',
+  ) );
 }
 add_action( 'widgets_init', 'strapped_widgets_init' );
